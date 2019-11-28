489d5492e51820c66fb8784f6b00c8de5cb8af62
Henry Tan <Henry.Tan@vudu.com>
2019-11-27 16:14:45 -0800

    camera example

---
+++ camera.py
@@ -0,0 +1,30 @@
+import cv2
+import time
+# Windows dependencies
+# - Python 2.7.6: http://www.python.org/download/
+# - OpenCV: http://opencv.org/
+# - Numpy -- get numpy from here because the official builds don't support x64:
+#   http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
+
+# Mac Dependencies
+# - brew install python
+# - pip install numpy
+# - brew tap homebrew/science
+# - brew install opencv
+
+cap = cv2.VideoCapture(0)
+now = time.clock()
+frames = 0
+while(True):
+    ret, frame = cap.read()
+    frames += 1
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+
+    cv2.imshow('frame', rgb)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        out = cv2.imwrite('capture.jpg', frame)
+        break
+
+print(f"{frames/(time.clock()-now)} fp/s" )
+cap.release()
+cv2.destroyAllWindows()

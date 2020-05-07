f996328e147ce752855a368a8fc5773861f4986c
Henry Tan <Henry.Tan@vudu.com>
2020-05-07 15:55:25 -0700

    code clean up

--- camera_lib.py
+++ camera_lib.py
@@ -1,8 +1,9 @@
-#this the python bindings for camera server support
-#the API calls camera server REST API
+# this the python bindings for camera server support
+# the API calls camera server REST API
 import requests
-response = requests.get('https://google.com/')
-print(f"google response: {response}")
+# response = requests.get('https://google.com/')
+# print(f"google response: {response}")
+
 
 class Camera():
     def __init__(self, url=None):
@@ -15,13 +16,13 @@ class Camera():
     def allocate_camera(self, url):
         print(f"=== init_camera ===")
         r = self.ping(url)
-        if r==200:
+        if r == 200:
             self.init = True
-            self.url =  url
+            self.url = url
         return r
 
     def free_camera(self):
-        print(f"=== init_camera ===")
+        print(f"=== free_camera ===")
         if self.init is True:
             self.init = False
             self.url = None
@@ -46,9 +47,9 @@ class Camera():
             return 404
         r = requests.get(url=f'{self.url}/start_live/{address}', cookies=self.session_cookie)
         # if r.status_code == 200:
-            # with open(path, 'wb') as f:
-            #     for chunk in r.iter_content(1024):
-            #         f.write(chunk)
+        # with open(path, 'wb') as f:
+        #     for chunk in r.iter_content(1024):
+        #         f.write(chunk)
         return r.status_code
 
     def stop_live(self):
@@ -57,11 +58,10 @@ class Camera():
             return 404
         r = requests.get(url=f'{self.url}/stop_live', cookies=self.session_cookie)
         # if r.status_code == 200:
-            # with open(path, 'wb') as f:
-            #     for chunk in r.iter_content(1024):
-            #         f.write(chunk)
+        # with open(path, 'wb') as f:
+        #     for chunk in r.iter_content(1024):
+        #         f.write(chunk)
         return r.status_code
-
 
     def _frame(self, path=None):
         # example to read file
@@ -89,33 +89,6 @@ class Camera():
                         f.write(chunk)
         return r.status_code
 
-    '''
-    def match(url, image_path, region=[0,0,1280,1080], method=None):
-        print(f"=== match ===")
-        data = open(image_path, 'rb').read()
-        r = requests.post(url=f'{url}/match', data=data,
-                                 headers={'Content-Type': 'application/octet-stream',
-                                          'Region': f'{region[0]},{region[1]},{region[2]},{region[3]}'})
-        return r.status_code
-    
-    def ocr(url, region=[0,0,1280,1080]):
-        print(f"=== ocr ===")
-        r = requests.post(url=f'{url}/ocr',
-                          headers={'Region': f'{region[0]},{region[1]},{region[2]},{region[3]}'})
-        ocr_result = ""
-        if r.status_code == 200:
-            for chunk in r.iter_content(1024):
-                ocr_result = ocr_result + chunk
-        return r.status_code, ocr_result
-    
-    def detect_video_motion(url):
-        print(f"=== detect_video_motion ===")
-        r = requests.get(url=f'{url}/detect_video_motion', stream=True)
-        motion_result = False
-        if r.status_code == 200:
-            motion_result = True
-        return r.status_code, motion_result
-    '''
 #
 # print(f"=== test match ===")
 # data = open('./images/test/playerBack.png', 'rb').read()
@@ -127,7 +100,6 @@ class Camera():
 # assert base64.b64decode(res.json()['data'][len('data:application/octet-stream;base64,'):]) == data
 
 
-
 # example to receive data from server
 # https://answers.splunk.com/answers/306952/return-binary-data-such-as-images-via-rest-api-or.html
 
@@ -136,7 +108,9 @@ class Camera():
 
 
 if __name__ == "__main__":
+
     import os
+
     if not os.path.exists('output'):
         os.makedirs('output')
     # URL = 'http://localhost:5000'
@@ -145,11 +119,11 @@ if __name__ == "__main__":
     status = cam.allocate_camera(URL)
     print(f"response: {status}")
 
-    status = cam._frame(path = 'output/_frame.png')
+    status = cam._frame(path='output/_frame.png')
     print(f"response: {status}")
     assert 200 == status
 
-    status = cam.get_frame(path = 'output/new_frame.png')
+    status = cam.get_frame(path='output/new_frame.png')
     print(f"response: {status}")
     assert 200 == status
 
@@ -167,4 +141,3 @@ if __name__ == "__main__":
 
     # status, ret = match('./images/test/playerBack.png')
     # status, ret = ocr([0,0,320,320])
-

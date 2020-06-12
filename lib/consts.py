e7cdb305f1253e9804f52ad92010624a5578285d
Henry Tan <Henry.Tan@vudu.com>
2020-06-11 19:09:21 -0700

    sync up VirtualSolution

---
+++ lib/consts.py
@@ -0,0 +1,16 @@
+MENU_SCREEN_TABS = ["My Vudu", "Spotlight", "Free", "Movies", "TV", "Search", "Settings"]
+
+DETAIL_PAGE_BUTTONS =["Watch", "Rate", "Resume", "Watch Trailer", "Watch Free", "Rent / Buy", "Add to List", "Rent", "Buy"]
+
+SETTINGS_PAGE_TABS = ["My Account","Family Settings", "Closed Captioning", "Playback Quality", "Autoplay Settings", "Accessibility", "About"]
+
+QUALITY_UHD = "UHD"
+QUALITY_HDX = "HDX"
+QUALITY_SD = "SD"
+QUALITY_ANY = 'ANY"'
+QUALITY = [QUALITY_UHD, QUALITY_HDX, QUALITY_SD, QUALITY_ANY]
+
+OWN_RENT_TITLE = "Own or Rent"
+BUY_RENT_TITLE = "Buy or Rent"
+RENT_BUY_TITLE = "Rent Or Buy"
+PRE_ORDER_RENT_TITLE = "Pre-Order or Rent"
---
+++ lib/roku_images.py
@@ -0,0 +1,62 @@
+MAIN_MENU_SEL_LEFT = "./images/Roku/main_menu_sel_l.png"
+MAIN_MENU_SEL_RIGHT = "./images/Roku/main_menu_sel_r.png"
+
+# playback page images
+PLAYER_BACK = "./images/Roku/playerBack.png"
+PLAYER_15_SECS = "./images/Roku/player15secs.png"
+PLAYER_BUTTON_HDX_AT_PROGRESS_BAR = "./images/Roku/playerButtonHDXAtProgressBar.png"
+PLAYER_BUTTON_UHD_AT_PROGRESS_BAR =  "./images/Roku/playerButtonUHDAtProgressBar.png"
+PLAYER_BUTTON_HDX_AT_PROGRESS_BAR = "./images/Roku/playerButtonHDXAtProgressBar.png"
+PLAYER_BUTTON_SD_AT_PROGRESS_BAR = "./images/Roku/playerButtonSDAtProgressBar.png"
+PLAYER_SYMBOL_PROGRESS_BAR_UHD_UNHI = "./images/Roku/playerSymbolAtProgressBarUHDUnhi.png"
+PLAYER_SYMBOL_PROGRESS_BAR_HDX_UNHI = "./images/Roku/playerSymbolAtProgressBarHDXUnhi.png"
+PLAYER_SYMBOL_PROGRESS_BAR_SD_UNHI =  "./images/Roku/playerSymbolAtProgressBarSDUnhi.png"
+PLAYER_CHAPTER_GRID = "./images/Roku/playerChapterGrid.png"
+PLAYER_RW_BUTTON = "./images/Roku/playerRWButton.png"
+KM_PLAYER_FF_BUTTON = "./images/Roku/playerFFButton.png"
+PLAYER_CC = "./images/Roku/playerCC.png"
+PLAYER_CC_UNHI = "./images/Roku/playerCCUnHi.png"
+PLAYER_CC_ENG = "./images/Roku/playerCCEng.png"
+PLAYER_CC_ENG_UNHI = "./images/Roku/playerCCEngUnHi.png"
+FAMILY_PLAY_SYMBOL_FOCUS = "./images/Roku/familyPlaySymbolFocus.png"
+FAMILY_PLAY_SYMBOL_UNFOCUS = "./images/Roku/familyPlaySymbolUnfocus.png"
+FAMILY_PLAY_SYMBOL_ON_FOCUS = "./images/Roku/familyPlaySymbolOnFocus.png"
+FAMILY_PLAY_SYMBOL_ON_UNFOCUS = "./images/Roku/familyPlaySymbolOnUnfocus.png"
+
+PLAYER_SYMBOL_UHD = "./images/Roku/playBackUHDIcon.png"
+PLAYER_SYMBOL_HDX = "./images/Roku/playBackHDXIcon.png"
+PLAYER_SYMBOL_SD = "./images/Roku/playBackSDIcon.png"
+
+PLAYBACK_CHAPTER_CHAPTER_1 = "./images/Roku/playback_chapter_chapter_1.png"
+PLAYBACK_CHAPTER_CHAPTER_1_UNHI = "./images/Roku/playback_chapter_chapter_1_unhi.png"
+PLAYBACK_CHAPTER_CHAPTER = "./images/Roku/playback_chapter_chapter.png"
+PLAYBACK_CHAPTER_CHAPTER_UNHI = "./images/Roku/playback_chapter_chapter_unhi.png"
+
+# detail page images
+DETAIL_TAB_SEL = "./images/Roku/detailPageTabSel.png"
+
+# search page images
+SEARCH_SUGGESTION_SEL_LEFT = "./images/Roku/search_suggestion_sel_l.png"
+SEARCH_SUGGESTION_SEL_RIGHT = "./images/Roku/search_suggestion_sel_r.png"
+
+# purchase popup images
+PURCHASE_POPUP_UHD_UH = "./images/Roku/purchasePopupUHD.png"
+PURCHASE_POPUP_HDX_UH = "./images/Roku/purchasePopupHDXUH.png"
+PURCHASE_POPUP_SD_UH = "./images/Roku/purchasePopupSDUH.png"
+PURCHASE_POPUP_SEL = "./images/Roku/purchasePopupSel.png"
+PURCHASE_POPUP_PRICE_20X36 = "./images/Roku/purchasePopupPrice20x36.png"
+MOVIE_PURCHASE_POPUP_CANCEL_BUTTON = "./images/Roku/moviePurchasePopupCancel.png"
+PURCHASE_POPUP_RENT = './images/Roku/priceRent.png'
+PURCHASE_POPUP_BUY_UH = './images/Roku/purchaseBuyUH.png'
+PURCHASE_ORANGE_DISCOUNT = './images/Roku/purchaseOrangeDiscount.png'
+PURCHASE_ORANGE_DISCOUNT_UH = './images/Roku/purchaseOrangeDiscountUH.png'
+ORANGE_DISCOUNT_PRICE_DETAILS = './images/Roku/orangePrice.png'
+PURCHASE_POPUP_BUY_4K = './images/Roku/priceBuy4K.png'
+PURCHASE_POPUP_BUY_HD = './images/Roku/priceBuyHD.png'
+PURCHASE_POPUP_BUY_SD = './images/Roku/priceBuySD.png'
+PURCHASE_POPUP_RENT_4K = './images/Roku/priceRent4K.png'
+PURCHASE_POPUP_RENT_HD = './images/Roku/priceRentHD.png'
+PURCHASE_POPUP_RENT_SD = './images/Roku/priceRentSD.png'
+PURCHASE_POPUP_PRICE_GRID_SEL = './images/Roku/purchasePriceSel.png'
+# settings page images
+SETTINGS_MENU_SEL = "./images/Roku/settings_menu_sel.png"

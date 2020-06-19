2aa8e8ebe0a3892ae24413103b6d183cbc4a5ac3
Henry Tan <Henry.Tan@vudu.com>
2020-06-19 12:05:23 -0700

    keep region device dependent

---
+++ lib/roku/regions.py
@@ -0,0 +1,39 @@
+from lib.vudu_image import Region
+# playback page regions
+REGION_PROGRESS_BAR = Region(x=0, y=630, right=1280, bottom=720)
+REGION_PROGRESS_BAR_TIME = Region(x=900, y=630, right=1280, bottom=720)
+REGION_PLAYBACK_PAGE_TITLE = Region(x=0, y=0, right=1280, bottom=90)
+REGION_CHAPTER_GRID = Region(115, 133, 1161, 577)
+
+# detail page regions
+REGION_DETAIL_BUTTONS = Region(80, 420, 330, 700)
+REGION_DETAILS_PAGE_TEXT = Region(x=316, y=109, right=1210, bottom=650)
+REGION_DETAILS_PAGE_TITLE = Region(x=316, y=109, right=1180, bottom=167)
+REGION_DETAILS_PAGE_TITLE1 = Region(x=316, y=149, right=1180, bottom=196)
+REGION_DETAILS_PAGE_TITLE2 = Region(x=316, y=105, right=1180, bottom=210)
+
+
+# search page regions
+REGION_SEARCH_TITLE = Region(x=870, y=195, right=1100, bottom=257)
+REGION_SEARCH_SUGGESTIONS = Region(x=0, y=460, right=330, bottom=720)
+
+# purchase popup regions
+REGION_PURCHASE_POPUP = Region(x=200, y=110, right=1080, bottom=600)
+REGION_PURCHASE_POPUP_TEXT = Region(x=322, y=109, right=1210, bottom=700)
+REGION_PURCHASE_POPUP_TITLE = Region(x=450, y=158, right=800, bottom=191)
+REGION_PURCHASE_POPUP_MOVIE_TITLE = Region(x=450, y=190, right=800, bottom=230)
+REGION_THANK_YOU_PURCHASE_POPUP_TITLE = Region(x=450, y=158, right=742, bottom=191)
+REGION_PURCHASE_POPUP_SUB_TITLE = Region(x=590, y=232, right=830, bottom=264)
+REGION_PURCHASE_POPUP_GRID_TEXT = Region(x=553, y=279, right=1210, bottom=700)
+REGION_PURCHASE_POPUP_GRID_PRICE = Region(x=550, y=270, right=1048, bottom=500)
+REGION_PURCHASE_POPUP_GRID_VIDEO_QAULITY = Region(x=450, y=230, right=550, bottom=500)
+REGION_SINGLE_PRICE_GRID = Region(x=278, y=155, right=977, bottom=623)
+
+
+# confirm purchase popup regions
+REGION_CONFIRM_PURCHASE_POPUP_TEXT = Region(x=440, y=145, right=1018, bottom=435)
+REGION_CONFIRM_PURCHASE_POPUP_TITLE = Region(x=440, y=145, right=742, bottom=200)
+
+
+# settings page regions
+REGION_SETTINGS_TABS = Region(x=100, y=228, right=390, bottom=600)

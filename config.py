aa01f2f0626cf3e1bcf171f472621759a3c41da4
shaokangtan <shaokangtan@yahoo.com>
2020-06-24 10:57:41 -0700

    add support windows platform

--- config.py
+++ config.py
@@ -1,2 +1,20 @@
-CAMERA_ID = 0
-INPUT_DEVICE = "avfoundation"
+# ea tester will has its own camera ip, roku ip.
+# camera id and input device will be based on the camera server's platform. currently, we support Windows and Mac OS.
+CAMERA_IP = "192.168.8.152:33"
+ROKU_IP = "192.168.8.201:8060"
+
+import platform
+
+system = platform.system()
+print(f"running on {system} platform")
+
+if system == 'Windows':
+    # Windows
+    CAMERA_ID = "video=\"UHD Capture\""
+    INPUT_DEVICE = "dshow"
+elif system == 'Darwin':
+    # Mac OS
+    CAMERA_ID = "\"0:none\""
+    INPUT_DEVICE = "avfoundation"
+else:
+    assert False, "platform not supported"
